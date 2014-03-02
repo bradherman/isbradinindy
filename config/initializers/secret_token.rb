1d4b9958f17c2e0a80f4a8fb99c2ac0e9f7926da
Bradley Herman <bradley.t.herman@gmail.com>
2014-03-02 14:04:38 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Isbradinindy::Application.config.secret_key_base = '7a7f501fce251cffdde247bb3cc669b7ff7a3cbabe64da3a211d20ec03aa3232341dc6bd057b537b2444151a4dcf992641cc6b0bb4d9be3a68005ec210fabe5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Isbradinindy::Application.config.session_store :cookie_store, key: '_isbradinindy_session'
