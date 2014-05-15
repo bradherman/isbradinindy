5036b89eec938320c8d91e18528d0af034000f7d
Bradley Herman <bradley.t.herman@gmail.com>
2014-05-14 19:37:53 -0700

    Facebook tweaks

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,23 @@
 class Page < Notifier
+  attr_accessor :page, :token, :account
+
+  def initialize(message)
+    super
+  end
+
   def send!
-    # FBOOK.feed!(message: message)
+    page.feed!(message: message)
+  end
+
+  def page
+    @page ||= FbGraph::User.me(@tok)
+  end
+
+  def token
+    @token ||= acct.access_token
+  end
+
+  def account
+    @account ||= FBOOK.accounts.detect{|x| x.name == 'Isbradinindy'}
   end
 end
